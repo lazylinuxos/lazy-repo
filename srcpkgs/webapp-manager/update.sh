dca15ad866d1730e33308be171fb60a53191dcef
Karen Sargsyan <karyan40024@gmail.com>
2026-03-01 14:44:15 +0400

    Update webapp-manager

--- srcpkgs/webapp-manager/update.sh
+++ srcpkgs/webapp-manager/update.sh
@@ -7,7 +7,11 @@ __dir="$(dirname "${BASH_SOURCE[0]}")"
 
 GH_REPO="linuxmint/webapp-manager"
 
-LATEST_VERSION=$(gh release list --repo ${GH_REPO} --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+LATEST_VERSION=$(
+  gh api repos/${GH_REPO}/tags \
+  --jq '.[].name | select(startswith("master.") | not)' \
+  | head -n1
+)
 export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
@@ -15,7 +19,7 @@ printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CU
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
 # No preprepped checksum files, need to download the binary and calculate it myself
-gh release download -R ${GH_REPO} --archive=tar.gz --output "${VERSION}.tar.gz"
+curl -L "https://github.com/${GH_REPO}/archive/refs/tags/${LATEST_VERSION}.tar.gz" -o "${VERSION}.tar.gz"
 export SHA256=$(sha256sum ./${VERSION}.tar.gz | cut -d ' ' -f1 )
 rm ./${VERSION}.tar.gz
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
